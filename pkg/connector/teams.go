1439229d8575e5609abda446a766acb72a220169
Vlada Nevyhosteny <vladimir.nevyhosteny@applifting.cz>
2023-11-08 20:22:32 +0100

    feat: grant team membership to user

--- pkg/connector/teams.go
+++ pkg/connector/teams.go
@@ -10,7 +10,9 @@ import (
 	ent "github.com/conductorone/baton-sdk/pkg/types/entitlement"
 	grant "github.com/conductorone/baton-sdk/pkg/types/grant"
 	rs "github.com/conductorone/baton-sdk/pkg/types/resource"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.mongodb.org/atlas-sdk/v20231001002/admin"
+	"go.uber.org/zap"
 )
 
 type teamBuilder struct {
@@ -141,4 +143,74 @@ func (o *teamBuilder) Grants(ctx context.Context, resource *v2.Resource, pToken 
 	}
 
 	return nil, nextPage, nil, nil
+}
+
+func (o *teamBuilder) Grant(ctx context.Context, principal *v2.Resource, entitlement *v2.Entitlement) (annotations.Annotations, error) {
+	l := ctxzap.Extract(ctx)
+
+	if principal.Id.ResourceType != userResourceType.Id {
+		err := fmt.Errorf("mongodb connector: only users can be granted to teams")
+
+		l.Warn(
+			err.Error(),
+			zap.String("principal_id", principal.Id.Resource),
+			zap.String("principal_type", principal.Id.ResourceType),
+		)
+
+		return nil, err
+	}
+
+	_, _, err := o.client.TeamsApi.AddTeamUser(
+		ctx,
+		principal.ParentResourceId.Resource,
+		entitlement.Resource.Id.Resource,
+		&[]admin.AddUserToTeam{
+			{
+				Id: principal.Id.Resource,
+			},
+		}).Execute()
+	if err != nil {
+		err := wrapError(err, "failed to add user to team")
+
+		l.Error(
+			err.Error(),
+			zap.String("team_id", entitlement.Resource.Id.Resource),
+			zap.String("user_id", principal.Id.Resource),
+		)
+
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (o *teamBuilder) Revoke(ctx context.Context, grant *v2.Grant) (annotations.Annotations, error) {
+	l := ctxzap.Extract(ctx)
+
+	if grant.Principal.Id.ResourceType != userResourceType.Id {
+		err := fmt.Errorf("mongodb connector: only users can be revoked from teams")
+
+		l.Warn(
+			err.Error(),
+			zap.String("principal_id", grant.Principal.Id.Resource),
+			zap.String("principal_type", grant.Principal.Id.ResourceType),
+		)
+
+		return nil, err
+	}
+
+	_, err := o.client.TeamsApi.RemoveTeamUser(ctx, grant.Principal.ParentResourceId.Resource, grant.Entitlement.Resource.Id.Resource, grant.Principal.Id.Resource).Execute()
+	if err != nil {
+		err := wrapError(err, "failed to remove user from team")
+
+		l.Error(
+			err.Error(),
+			zap.String("team_id", grant.Entitlement.Resource.Id.Resource),
+			zap.String("user_id", grant.Principal.Id.Resource),
+		)
+
+		return nil, err
+	}
+
+	return nil, nil
 }
