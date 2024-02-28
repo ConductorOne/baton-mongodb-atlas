9429fafc7d625471b84a28d45ed94c55d0758f5f
mchavez <miguel_chavez_m@hotmail.com>
2024-02-27 19:31:20 -0600

    Update Actions

--- pkg/connector/projects.go
+++ pkg/connector/projects.go
@@ -192,6 +192,7 @@ func (p *projectBuilder) Grants(ctx context.Context, resource *v2.Resource, pTok
 }
 
 func (p *projectBuilder) GrantUsers(ctx context.Context, resource *v2.Resource, page int) ([]*v2.Grant, int, error) {
+	fmt.Println("--- GrantUsers ---")
 	members, _, err := p.client.ProjectsApi.ListProjectUsers(ctx, resource.Id.Resource).PageNum(page).ItemsPerPage(resourcePageSize).IncludeCount(true).Execute()
 	if err != nil {
 		return nil, 0, wrapError(err, "failed to list project users")
@@ -226,6 +227,7 @@ func (p *projectBuilder) GrantUsers(ctx context.Context, resource *v2.Resource, 
 }
 
 func (p *projectBuilder) GrantDatabaseUsers(ctx context.Context, resource *v2.Resource, page int) ([]*v2.Grant, int, error) {
+	fmt.Println("--- GrantDatabaseUsers ---")
 	members, _, err := p.client.DatabaseUsersApi.ListDatabaseUsers(ctx, resource.Id.Resource).PageNum(page).ItemsPerPage(resourcePageSize).IncludeCount(true).Execute()
 	if err != nil {
 		return nil, 0, wrapError(err, "failed to list project database users")
@@ -245,8 +247,10 @@ func (p *projectBuilder) GrantDatabaseUsers(ctx context.Context, resource *v2.Re
 }
 
 func (p *projectBuilder) Grant(ctx context.Context, principal *v2.Resource, entitlement *v2.Entitlement) (annotations.Annotations, error) {
+	fmt.Println("--- Grant Organization ---")
 	l := ctxzap.Extract(ctx)
-
+	fmt.Println(principal.Id.ResourceType)
+	fmt.Println(userResourceType.Id)
 	if principal.Id.ResourceType != userResourceType.Id {
 		err := fmt.Errorf("mongodb connector: only users can be granted to projects")
 
